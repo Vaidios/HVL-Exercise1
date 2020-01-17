7e44ece655b9dfdf1c10660f50e418941831e6e2
Vaidios <25388414+Vaidios@users.noreply.github.com>
2020-01-17 12:37:27 +0100

    Initial commit

---
+++ app/src/main/java/com/example/firstapp/DisplayMessageActivity.kt
@@ -0,0 +1,20 @@
+package com.example.firstapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class DisplayMessageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+
+        val message = intent.getStringExtra(EXTRA_MSG)
+
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
+
+    }
+}
